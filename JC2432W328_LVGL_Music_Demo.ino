c9798b01f6145dd65f973af00c33a3855071783a
jmaniac <sathishguru@gmail.com>
2026-01-18 14:52:41 +0530

    Initial commit

---
+++ JC2432W328_LVGL_Music_Demo.ino
@@ -0,0 +1,143 @@
+/*Using LVGL with Arduino requires some extra steps:
+ *Be sure to read the docs here: https://docs.lvgl.io/master/integration/framework/arduino.html  */
+
+#include <lvgl.h>
+
+#if LV_USE_TFT_ESPI
+#include <TFT_eSPI.h>
+#endif
+
+#include "CST820.h"
+#include <Wire.h>
+
+/*To use the built-in examples and demos of LVGL uncomment the includes below respectively.
+ *You also need to copy `lvgl/examples` to `lvgl/src/examples`. Similarly for the demos `lvgl/demos` to `lvgl/src/demos`.
+ *Note that the `lv_examples` library is for LVGL v7 and you shouldn't install it for this version (since LVGL v8)
+ *as the examples and demos are now part of the main LVGL library. */
+
+//#include <examples/lv_examples.h>
+#include <demos/lv_demos.h>
+
+/*Set to your screen resolution and rotation*/
+#define TFT_HOR_RES   240
+#define TFT_VER_RES   320
+#define TFT_ROTATION  LV_DISPLAY_ROTATION_90
+
+#define I2C_SDA 33
+#define I2C_SCL 32
+#define TP_RST 25
+#define TP_INT 21
+
+/*LVGL draw into this buffer, 1/10 screen size usually works well. The size is in bytes*/
+#define DRAW_BUF_SIZE (TFT_HOR_RES * TFT_VER_RES / 10 * (LV_COLOR_DEPTH / 8))
+uint32_t draw_buf[DRAW_BUF_SIZE / 4];
+
+CST820 touch(I2C_SDA, I2C_SCL, TP_RST, TP_INT);
+
+// lv_indev_t* indev;  // touch device
+
+#if LV_USE_LOG != 0
+void my_print( lv_log_level_t level, const char * buf )
+{
+    LV_UNUSED(level);
+    Serial.println(buf);
+    Serial.flush();
+}
+#endif
+
+/* LVGL calls it when a rendered image needs to copied to the display*/
+void my_disp_flush( lv_display_t *disp, const lv_area_t *area, uint8_t * px_map)
+{
+    // uint16_t x1 = area->x1 + ST7789_X_OFFSET;
+    // uint16_t y1 = area->y1 + ST7789_Y_OFFSET;
+    // uint16_t x2 = area->x2 + ST7789_X_OFFSET;
+    // uint16_t y2 = area->y2 + ST7789_Y_OFFSET;
+
+    // st7789_set_window(x1, y1, x2, y2);
+
+    // uint32_t w = lv_area_get_width(area);
+    // uint32_t h = lv_area_get_height(area);
+
+    // st7789_push_pixels((uint16_t *)px_map, w * h);
+
+    lv_display_flush_ready(disp);
+}
+
+/*Read the touchpad*/
+void my_touchpad_read( lv_indev_t * indev, lv_indev_data_t * data )
+{
+    bool touched;
+    uint8_t gesture;
+    uint16_t touchX, touchY;
+
+    touched = touch.getTouch(&touchX, &touchY, &gesture);
+
+    if (!touched)
+    {
+        data->state = LV_INDEV_STATE_REL;
+    }
+    else
+    {
+        data->state = LV_INDEV_STATE_PR;
+
+        /*Set the coordinates*/
+        data->point.x = touchX;
+        data->point.y = touchY;
+    }
+}
+
+/*use Arduinos millis() as tick source*/
+static uint32_t my_tick(void)
+{
+    return millis();
+}
+
+void setup()
+{
+    String LVGL_Arduino = "Hello Arduino! ";
+    LVGL_Arduino += String('V') + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
+
+    Serial.begin( 115200 );
+    Serial.println( LVGL_Arduino );
+
+    lv_init();
+
+    /*Set a tick source so that LVGL will know how much time elapsed. */
+    lv_tick_set_cb(my_tick);
+
+    /* register print function for debugging */
+#if LV_USE_LOG != 0
+    lv_log_register_print_cb( my_print );
+#endif
+
+    lv_display_t * disp;
+#if LV_USE_TFT_ESPI
+    /*TFT_eSPI can be enabled lv_conf.h to initialize the display in a simple way*/
+    disp = lv_tft_espi_create(TFT_HOR_RES, TFT_VER_RES, draw_buf, sizeof(draw_buf));
+    lv_display_set_rotation(disp, TFT_ROTATION);
+
+#else
+    /*Else create a display yourself*/
+    disp = lv_display_create(TFT_HOR_RES, TFT_VER_RES);
+    lv_display_set_flush_cb(disp, my_disp_flush);
+    lv_display_set_buffers(disp, draw_buf, NULL, sizeof(draw_buf), LV_DISPLAY_RENDER_MODE_PARTIAL);
+#endif
+
+    /*Initialize the (dummy) input device driver*/
+    lv_indev_t * indev = lv_indev_create();
+    lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER); /*Touchpad should have POINTER type*/
+    lv_indev_set_read_cb(indev, my_touchpad_read);
+
+    // lv_obj_set_style_bg_color(lv_scr_act(),
+                        //   lv_color_hex(0xFF0000), 0);
+
+    touch.begin();
+    lv_demo_music();
+    Serial.println( "Setup done" );
+}
+
+void loop()
+{
+    lv_timer_handler(); /* let the GUI do its work */
+    delay(5); /* let this time pass */
+}
---
+++ Readme.md
@@ -0,0 +1,18 @@
+The LVGL Music Demo made to work with JC2432W328
+
+Use the lvgl_conf.h for the refernce on enabling the LVGL flags.
+
+Preview
+
+Portrait
+In lv_conf.h
+`#define LV_DEMO_MUSIC_LANDSCAPE 0`
+
+
+Landscape
+
+In lv_conf.h
+`#define LV_DEMO_MUSIC_LANDSCAPE 1`
+
+In Sketch
+`#define TFT_ROTATION  LV_DISPLAY_ROTATION_90`
